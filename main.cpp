76b768aa7686da2abaf0bb19efc3189c48844347
Juan Lopez <naezzujl19@gmail.com>
2022-11-07 20:52:56 +0000

    Nueva linea de codigo

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout<< "Hola mundo desde un contenedor de Docker!"<<endl;
+    cout<< "Nueva linea de codigo para GIT!"<<endl;
     return 0;
 }
